8b4d4224b4e68cf7453f5174bbf5181c52f95b8c
lou921944921 <921944921@qq.com>
2016-01-07 12:29:59 +0800

    Update and rename .gitignore to .rinetd.sh

---
+++ .rinetd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+apt-get install rinetd
+sed -i '1a 0.0.0.0 443 192.184.12.180 443' /etc/rinetd.conf
+pkill rinetd  
+rinetd -c /etc/rinetd.conf
+sed -i '1a rinetd -c /etc/rinetd.conf' /etc/rc.local
