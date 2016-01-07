1406e27cfd010be30a4f0841fa98927a4dbb5b51
lou921944921 <921944921@qq.com>
2016-01-07 14:33:41 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install python-gevent python-pip &&y
+sudo apt-get -y install python-gevent python-pip 
 pip install shadowsocks
 touch /etc/ss-config.json  
 echo '{"server":"0.0.0.0" "server_port":8388 "local_port":1080 "password":"123456" "timeout":600 "method":"aes-256-cfb"}' > /etc/ss-config.json
