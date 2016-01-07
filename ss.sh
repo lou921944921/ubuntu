5051265c9cb0b3d4af078c1c5114479e884b5c9e
lou921944921 <921944921@qq.com>
2016-01-07 15:31:25 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 touch /etc/ss-config.json  
 echo '{"server":"0.0.0.0", "server_port":8388, "local_port":1080, "password":"123456", "timeout":600, "method":"aes-256-cfb"}' > /etc/ss-config.json
+sudo chmod 755 /etc/ss-config.json
 sudo apt-get update
 sudo apt-get -y install python-gevent python-pip 
 pip install shadowsocks
-/usr/local/bin/ssserver -c /etc/ss-config.json
+sudo apt–get install python–m2crypto
+sudo ssserver -c /etc/ss-config.json -d start
 sed -i '1a /usr/local/bin/ssserver -c /etc/ss-config.json' /etc/rc.local
