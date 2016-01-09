868f8ffa7eb9adaeee2d5da7e97e3000d5178f4f
lou921944921 <921944921@qq.com>
2016-01-09 15:19:16 +0800

    Update rinetd.sh

--- rinetd.sh
+++ rinetd.sh
@@ -4,4 +4,4 @@ apt-get install rinetd
 sed -i '1a 0.0.0.0 443 192.184.12.180 443' /etc/rinetd.conf
 pkill rinetd  
 rinetd -c /etc/rinetd.conf
-sed -i '1a rinetd -c /etc/rinetd.conf' /etc/rc.local
+sed -i '1a /usr/sbin/rinetd -c /etc/rinetd.conf' /etc/rc.local
