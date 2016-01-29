d0aac0fdb679fb01333cec4fc492b1979b9d861d
Nines <921944921@qq.com>
2016-01-29 18:12:47 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 touch /etc/ss-config.json  
-echo '{"server":"0.0.0.0", "server_port":8388, "local_port":1080, "password":"123456", "timeout":600, "method":"aes-256-cfb"}' > /etc/ss-config.json
+echo '{
+    "server": "0.0.0.0",
+    "server_port": 8388,
+    "local_port": 1080,
+    "password": "123456",
+    "timeout": 600,
+    "method": "aes-256-cfb",
+}' > /etc/ss-config.json
 sudo chmod 755 /etc/ss-config.json
 sudo apt-get update
 sudo apt-get -y install python-gevent python-pip 
